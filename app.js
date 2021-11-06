29e436728c5ca4ca139dc9c60b03c457204c195d
Emmanuel Rapoo <aniemmanuelrapo@gmail.com>
2021-11-06 02:06:16 +0100

    working on ejs

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine ejs
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
     // res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 })
 
 //redirect
-app.get('/about-me', (req, res) => {
-    res.redirect('/about')
+// app.get('/about-me', (req, res) => {
+//     res.redirect('/about')
+// })
+
+app.get('/blogs/create', (req, res) => {
+    res.render()
 })
 
 //404 Page
 // express runs code top to bottom. position is important
 // this code should stay at the bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404')
 })
