cbc580ee10162502605a2bb42ee00bfa0ff22248
Emmanuel Rapoo <aniemmanuelrapo@gmail.com>
2021-11-09 12:08:32 +0100

    rounding up with node

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoute')
 
 //express app
 const app = express();
@@ -52,54 +52,7 @@ app.get('/about', (req, res) => {
 })
 
 //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create'})
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+app.use('/blogs', blogRoutes);
 
 //redirect
 // app.get('/about-me', (req, res) => {
@@ -118,3 +71,8 @@ app.use((req, res) => {
 //POST requests to create new data (eg a new blog)
 //DELETE requests to delete data(eg. delete a blog)
 //PUT request to update data (eg. update a blog)
+
+// MVC BASICS
+//Stands for Model, View, Controller
+//MVC is a way of structuring our code & files
+//Keeps code more modular, reusable & easier to read
